791f2dad8204d0fbed0eaf8ade2d034d740f64c7
dfezeu <dylanf02@gmail.com>
2026-02-09 14:29:32 -0500

    Create InvitePage component for collaboration invites

---
+++ frontend/app/invite/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function InvitePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Invite</h1>
+      <p>Invite collaborators or teammates.</p>
+    </main>
+  );
+}
