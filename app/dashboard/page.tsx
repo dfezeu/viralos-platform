f7f963fc17527e678421eb1c271e4558159bcb32
dfezeu <dylanf02@gmail.com>
2026-02-09 14:44:48 -0500

    Create DashboardPage component for user activity overview

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function DashboardPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Dashboard</h1>
+      <p>Overview of your Viralos activity.</p>
+    </main>
+  );
+}
