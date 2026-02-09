c5b65bd486b2f24159b9e93dea830ea236f8afea
dfezeu <dylanf02@gmail.com>
2026-02-09 18:50:27 -0500

    Delete backend/src/routes/backend directory

--- backend/src/routes/backend/src/routes/clipforge.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createClip } from "../controllers/clipforge.controller.js";
-
-const router = Router();
-
-router.post("/", createClip);
-
-export default router;
