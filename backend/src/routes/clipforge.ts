cbf01ab001e758f6d90db6ce62685656f13f99ce
dfezeu <dylanf02@gmail.com>
2026-02-09 18:48:46 -0500

    Delete backend/src/routes/clipforge.ts

--- backend/src/routes/clipforge.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createClip } from '../controllers/clipforge.controller';
-
-const router = Router();
-
-router.post('/create', createClip);
-
-export default router;
